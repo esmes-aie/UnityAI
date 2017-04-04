03befa13a8b5885fde62f7627b29bf08c3524b39
esmes <esmes@SEA-167631>
2017-04-04 14:19:27 -0700

    Applied some checks to handle holes in the graph.

--- Assets/GraphManager.cs
+++ Assets/GraphManager.cs
@@ -13,7 +13,9 @@ public class GraphManager : MonoBehaviour {
     private List<Transform> path;
     public int gridSize = 4;
     public float gridSpace = 1;
+    public bool genGridKeep = false;
     public bool generateGrid = true;
+    
 
 
     private static float diff(Transform a, Transform b)
@@ -76,7 +78,6 @@ public class GraphManager : MonoBehaviour {
         }
     }
 
-    // inside the GraphManager
 
 
     // Called by the editor when a serialized field is modified
@@ -86,48 +87,51 @@ public class GraphManager : MonoBehaviour {
         if (!generateGrid) return;
         generateGrid = false;
 
-        // cleanup (hack)
-        foreach (Transform child in transform)
-                UnityEditor.EditorApplication.delayCall += () =>
-                                    { DestroyImmediate(child.gameObject); };
+        if(!genGridKeep)
+        { 
+            foreach (Transform child in transform)
+                    UnityEditor.EditorApplication.delayCall += () =>
+                                        { DestroyImmediate(child.gameObject); };
 
-        // clear out my lists of references to destroyed objects
-        start.Clear();
-        end.Clear();
+            // clear out my lists of references to destroyed objects
+            start.Clear();
+            end.Clear();
 
-        // total transforms we need
-        Transform[] t_array = new Transform[gridSize * gridSize];
+            // total transforms we need
+            Transform[] t_array = new Transform[gridSize * gridSize];
 
-        // generate nodes and apply offsets
-        for (int i = 0; i < gridSize; ++i)
-        {
-            for (int j = 0; j < gridSize; ++j)
+            // generate nodes and apply offsets
+            for (int i = 0; i < gridSize; ++i)
             {
-                var t = new GameObject(GetInstanceID() + "_gwp_" + (i * gridSize + j));
-                t.transform.parent = transform;
-                t.transform.localPosition = new Vector3((i + 0.5f - gridSize / 2.0f) * gridSpace,
-                                                        (j + 0.5f - gridSize / 2.0f) * gridSpace,
-                                                         0);
-                t_array[i * gridSize + j] = t.transform;
+                for (int j = 0; j < gridSize; ++j)
+                {
+                    var t = new GameObject(GetInstanceID() + "_gwp_" + (i * gridSize + j));
+                    t.transform.parent = transform;
+                    t.transform.localPosition = new Vector3((i + 0.5f - gridSize / 2.0f) * gridSpace,
+                                                            (j + 0.5f - gridSize / 2.0f) * gridSpace,
+                                                             0);
+                    t_array[i * gridSize + j] = t.transform;
+                }
+            }
+
+            // generate connections
+            for(int n = 0; n < gridSize*gridSize; ++n)
+            {
+                // form an edge with the node above me
+                if(((n+1)%gridSize) !=  0)
+                {
+                    start.Add(t_array[n]);
+                    end.Add(t_array[n+1]);
+                }
+                // form an edge with the node to the right of me
+                if(n+gridSize < gridSize * gridSize)
+                {
+                    start.Add(t_array[n]);
+                    end.Add(t_array[n + gridSize]);
+                }
             }
         }
-
-        // generate connections
-        for(int n = 0; n < gridSize*gridSize; ++n)
-        {
-            // form an edge with the node above me
-            if(((n+1)%gridSize) !=  0)
-            {
-                start.Add(t_array[n]);
-                end.Add(t_array[n+1]);
-            }
-            // form an edge with the node to the right of me
-            if(n+gridSize < gridSize * gridSize)
-            {
-                start.Add(t_array[n]);
-                end.Add(t_array[n + gridSize]);
-            }
-        }
+        genGridKeep = true;
 
         if(pathStart != null && pathEnd != null)
         {
